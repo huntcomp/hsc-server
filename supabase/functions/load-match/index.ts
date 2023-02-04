ef22830a573d0ce9a1bdee83f589c541f4e7706e
viters <lukasz.r.szczesniak@gmail.com>
2023-02-04 23:16:02 +0100

    feat: init load-match

---
+++ supabase/functions/load-match/index.ts
@@ -0,0 +1,24 @@
+import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
+import { TextLineStream } from "https://deno.land/std@0.176.0/streams/mod.ts";
+
+const nre = new RegExp('(?<=name=")[^"]*');
+const vre = new RegExp('(?<=value=")[^"]*');
+
+serve(async (req) => {
+  const data: Record<string, string> = {};
+
+  if (req.body) {
+    const f = req.body.pipeThrough(new TextDecoderStream()).pipeThrough(new TextLineStream());
+    for await (const l of f) {
+      if (typeof l === 'string' && l.includes('MissionBagPlayer_')) {
+        const name = nre.exec(l);
+        const value = vre.exec(l);
+        if (name?.[0] != null && value?.[0] != null) {
+          data[name[0]] = value[0];
+        }
+      }
+    }
+  }
+
+  return new Response(JSON.stringify(data));
+});
